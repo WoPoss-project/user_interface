ea8a8d0261a361a3eb3dc9690ffac874d9ecb9b9
helenasabel <helena.b.sabel@gmail.com>
2019-10-22 12:21:52 +0200

    [content] added two pieces of news

--- news.php
+++ news.php
@@ -1,18 +1,65 @@
-<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>News - WoPoss</title>
-<?php include("ssi/head.html"); ?>
-        
-        
-    </head>
-    <body>
-        <?php include("ssi/menu.html"); ?>
-        <main>
-            <h1>News and Outreach</h1>
-            <div class="warning">This section is under construction</div>
-        </main>
-        <?php include("ssi/footer.html"); ?>
-    </body>
+<head>
+    <title>News - WoPoss</title>
+    <?php include("ssi/head.html"); ?>
+
+
+</head>
+<body>
+<?php include("ssi/menu.html"); ?>
+<main>
+    <h1>News and Outreach</h1>
+    <div class="news">
+    <div>
+            <article id="survey">
+                <h2>Survey about words of fear</h2>
+<img src="images/news/word-cloud.png" alt="word cloud multilingual words of fear" class="cloud">
+                <p>We are currently working on an exposition concerning the words of fear, especially
+                    those that have taken a positive value throughout time. We have individuated for now three types of
+                    words:</p>
+                <ol>
+                    <li>As in French <em>formidable</em>: etymologically related to the feeling of fear (Latin
+                        <em>formidabilis</em>
+                        ⇒ “inspiring fear, frightening, terrifying”), it is now mainly used with the meaning “great,
+                        fantastic”;
+                    </li>
+                    <li>As in English <em>terrific</em>: also etymologically related to fear (Latin <em>terreo</em> ⇒ “to terrorize”) it
+                        can
+                        now be used both with a positive (“amazing”) and a negative (“terrible”) meaning;
+                    </li>
+
+                    <li>As in French <em>terriblement</em>, German <em>schrecklich</em>, English <em>awfully</em>, adverbs etymologically related
+                        to
+                        fear and that can nowadays intensify an adjective in both a positive and a negative way.
+                    </li>
+                </ol>
+
+                <p>We would like to enlarge the list as much as possible with words matching these three categories
+                and coming from as many languages as possible. Do you happen to have any ideas and feel like you
+                would like to contribute to our list? If so, please feel free to contact us: <a href="mailto:woposs@unil.ch">woposs@unil.ch</a>
+                </p>
+            </article>
+            <article id="workshop"><h2>WoPoss will participate on the second DARIAH-CH workshop</h2>
+                <img src="https://dh.tcd.ie/dh/wp-content/uploads/2019/04/DARIAH-EU_logo_with_baseline_-300DPI-1024x482.png"
+                     alt="DARIAH logo"/>
+                <p>The University of Neuchâtel will hold in December the second DARIAH-CH workshop. The theme of
+                    this second edition is
+                    <em><a href="https://dariah-ch-ws19.sciencesconf.org/" target="_blank">Sharing the
+                            Experience: Workflows for the Digital Humanities</a></em>
+                    and Francesca Dell’Oro, Helena Bermúdez Sabel
+                    and Paola Marongiu will be there presenting a paper entitled “WoPoss: a Workflow for the
+                    Semantic Annotation of Modality in a Diachronic Corpus”.
+                </p>
+                <p><a href="https://dariah-ch-ws19.sciencesconf.org/294836" target="_blank">Our abstract</a> is
+                    already available, and please visit <a
+                            href="https://dariah-ch-ws19.sciencesconf.org/program" target="_blank">this link</a>
+                    to see the complete program.</p></article>
+        </div>
+        <aside><a class="twitter-timeline" href="https://twitter.com/woposs?ref_src=twsrc%5Etfw">Tweets by woposs</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </aside>
+    </div>
+</main>
+<?php include("ssi/footer.html"); ?>
+</body>
 </html>
