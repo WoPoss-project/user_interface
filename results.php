c8c238ee2a8e36742c0c17c500e9adadf00de95a
helenasabel <helena.b.sabel@gmail.com>
2019-12-13 15:34:08 +0100

    [data] added publications

--- results.php
+++ results.php
@@ -11,7 +11,16 @@
         <?php include("ssi/menu.html"); ?>
         <main>
             <h1>Outputs</h1>
-            <div class="warning">This section is under construction</div>
+            
+            <h2>Project milestones</h2>
+            <ul>
+            <li>Dell’Oro, Francesca (2019). <em>WoPoss guidelines for annotation</em>. Zenodo. <a href="https://zenodo.org/record/3560951" target="_blank">doi:10.5281/zenodo.3560951</a>.</li>
+            </ul>
+            <h2>Papers</h2>
+            <ul>
+            <li>Bermúdez Sabel, Helena (in press). “Digital tools for semantic annotation: the WoPoss use case”. <em>Bulletin de linguistique et des sciences du langage</em>, 30. 
+            [<a href="http://doi.org/10.5281/zenodo.3572410" target="_blank">Preprint</a>].</li>
+            </ul>
         </main>
         <?php include("ssi/footer.html"); ?>
     </body>
