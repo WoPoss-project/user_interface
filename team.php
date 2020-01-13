89a3e56dc03e6ac6666474ac676fd60659451a6f
helenasabel <helena.b.sabel@gmail.com>
2020-01-13 15:43:46 +0100

    [content] added Séverine’s bio

--- team.php
+++ team.php
@@ -1,116 +1,134 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>Team - WoPoss</title>
-        <?php include("ssi/head.html"); ?>
-    </head>
-    <body>
-        <?php include("ssi/menu.html"); ?>
-        <main id="team">
-            <h1>The team</h1>
-            <img src="images/team/team.jpg" alt="The WoPoss Team" class="team-pic" />
-            <p>The WoPoss team consists of three researchers funded by the SNSF. Thanks to the
-                generosity of the University of Lausanne, we have been able to hire a number of
-                students as assistants. They help us in the annotation task.</p>
-            <div class="ip">
-                <figure>
-                    <img src="images/team/francesca.jpg" alt="Francesca Dell’Oro" />
-                    <figcaption>Francesca Dell’Oro (PhD 2008)</figcaption>
-                </figure>
-                <p>I am SNSF assistant professor of historical linguistics at the University of Lausanne and
-                    the PI of the WoPoss project.</p>
-                <p>I am a historical linguist with a strong interest in semantics. My research and
-                    expertise focus on the following areas:</p>
+<head>
+    <title>Team - WoPoss</title>
+    <?php include("ssi/head.html"); ?>
+</head>
+<body>
+<?php include("ssi/menu.html"); ?>
+<main id="team">
+    <h1>The team</h1>
+    <img src="images/team/team.jpg" alt="The WoPoss Team" class="team-pic"/>
+    <p>The WoPoss team consists of three researchers funded by the SNSF. Thanks to the
+        generosity of the University of Lausanne, we have been able to hire a number of
+        students as assistants. They help us in the annotation task.</p>
+    <div class="ip">
+        <figure>
+            <img src="images/team/francesca.jpg" alt="Francesca Dell’Oro"/>
+            <figcaption>Francesca Dell’Oro (PhD 2008)</figcaption>
+        </figure>
+        <p>I am SNSF assistant professor of historical linguistics at the University of Lausanne and
+            the PI of the WoPoss project.</p>
+        <p>I am a historical linguist with a strong interest in semantics. My research and
+            expertise focus on the following areas:</p>
 
-                <ul>
-                    <li>the analysis of meaning –in particular modal meaning– from both a diachronic
-                        and a synchronic perspective as well as the linguistic study of the
-                        metaphor</li>
-                    <li>the development of complementary methods which combine computational tools
-                        and philological expertise and their application to ancient languages</li>
-                    <li>the epistemological analysis of key concepts in the history of
-                        linguistics</li>
-                    <li>the linguistic analysis of ancient documents –inscriptions, manuscripts– in
-                        their material context</li>
-                    <li>the methods of teaching languages, in particular Classical languages</li>
-                </ul>
-            </div><div class="members">
-                <div>
-                    <figure>
-                        <img src="images/team/helena.jpg" alt="Helena Bermúdez Sabel" />
-                        <figcaption>Helena Bermúdez Sabel (PhD 2019)</figcaption>
-                    </figure>
-                    <p>Helena Bermúdez Sabel holds a PhD in Medieval Studies from the Universidade
-                        de Santiago de Compostela. Her doctoral research involved the development of
-                        a digital edition model that enables the quantitative study of linguistic
-                        variation through the automatic comparison of witnesses. An
-                        implementation of the model was illustrated with a <a href="www.gl-pt.obdurodon.org" target="_blank">Galician-Portuguese
-                        secular poetry corpus</a>.</p>
-                    <p>Before UNIL, she worked at the Laboratorio de Innovación en Humanidades
-                        Digitales (Madrid, Spain). She was a researcher at an ERC-funded project
-                        focused on enabling the interoperability of poetic resources from European
-                        traditions via linked open data. She also has a solid experience as
-                        an instructor in Digital Humanities courses.</p>
-                    <p>Helena is interested in data modeling and the formalization of annotation
-                        schemes. She is proficient in XML technologies and linked data. Thanks to
-                        her work in this project, she is exploring further programming
-                        languages.</p>
-                    <p>At WoPoss, she’s contributing to the annotation model. She supervises the
-                        technical aspects of the annotation workflow, including the automation of
-                        corpus pre-processing and post-processing. She is the developer of this
-                        website, so <a href="mailto:wwwwoposs@unil.ch">let her know if you find
-                            anything amiss</a>.</p>
-                </div>
-                <div>
-                    <figure>
-                        <img src="images/team/paola.jpg" alt="Paola Marongiu" />
-                        <figcaption>Paola Marongiu (MA 2018)</figcaption>
-                    </figure>
-                    <p>Paola Marongiu is a first year PhD student in Linguistics at the University of
-                        Lausanne, Section des Sciences du Langage et de l’Information, and Assistant
-                        in the FNS project WoPoss. Her PhD thesis focuses on the analysis of the
-                        co-occurrence of modal markers in Latin, from both a quantitative and a
-                        qualitative point of view. Her research interests include Theoretical
-                        Linguistics, with a focus on Latin and Modality, Digital Humanities and
-                        Computational Linguistics. More specifically, in her PhD project she will
-                        employ computational methods and resources in order to investigate a
-                        theoretical problem on multiple perspectives.</p>
-                    <p>In 2018 she obtained a Master degree in Theoretical and Applied Linguistics
-                        from the University of Pavia, with a thesis focused on the conversion of the
-                        Index Thomisticus Treebank into the Universal Dependencies annotation style.</p>
-                    <p>She has a Bachelor in Modern Studies, obtained from the University of Bologna
-                        in 2016.</p>
-
-                </div>
-
-            </div>
-            <h2>Assistants</h2>
-            <div class="members_et">
-            <div>
+        <ul>
+            <li>the analysis of meaning –in particular modal meaning– from both a diachronic
+                and a synchronic perspective as well as the linguistic study of the
+                metaphor
+            </li>
+            <li>the development of complementary methods which combine computational tools
+                and philological expertise and their application to ancient languages
+            </li>
+            <li>the epistemological analysis of key concepts in the history of
+                linguistics
+            </li>
+            <li>the linguistic analysis of ancient documents –inscriptions, manuscripts– in
+                their material context
+            </li>
+            <li>the methods of teaching languages, in particular Classical languages</li>
+        </ul>
+    </div>
+    <div class="members">
+        <div>
             <figure>
-            <img src="images/team/kelvin.jpg" alt="Kelvin Bruggmann"/>
-            <figcaption>Kelvin Bruggmann</figcaption>
+                <img src="images/team/helena.jpg" alt="Helena Bermúdez Sabel"/>
+                <figcaption>Helena Bermúdez Sabel (PhD 2019)</figcaption>
             </figure>
-            <p>Kelvin Bruggmann is a Bachelor (BA) student at the University of Lausanne. His interests are multiple but centered on Latin language and literature and (historical) linguistics.
-             He wants to develop and acquire new abilities and skills in the fields he is studying. His main goal is to better understand ancient cultures, including their languages, in order to better
-              understand our current world. For all those reasons, he is taking part and contributing to this project.</p>
-            </div>
-            <div>
-             <figure>
-                        <img src="images/team/mael.jpg" alt="Maël Graa" />
-                        <figcaption>Maël Graa (BA 2018)</figcaption>
-                    </figure>
-                    
-                   <p>Maël Graa obtained a Bachelor of Arts in History and Latin and he decided to deepen his knowledge in these two fields during his post-graduate training.
-                   He is currently pursuing a Master degree at the Université de Lausanne, with an additional specialization in Discourse and Public Communication Analysis. His Master Thesis concerns the Latin poetry by Théodore de Bèze.</p>
-            </div>
-                        <div>
+            <p>Helena Bermúdez Sabel holds a PhD in Medieval Studies from the Universidade
+                de Santiago de Compostela. Her doctoral research involved the development of
+                a digital edition model that enables the quantitative study of linguistic
+                variation through the automatic comparison of witnesses. An
+                implementation of the model was illustrated with a <a href="www.gl-pt.obdurodon.org" target="_blank">Galician-Portuguese
+                    secular poetry corpus</a>.</p>
+            <p>Before UNIL, she worked at the Laboratorio de Innovación en Humanidades
+                Digitales (Madrid, Spain). She was a researcher at an ERC-funded project
+                focused on enabling the interoperability of poetic resources from European
+                traditions via linked open data. She also has a solid experience as
+                an instructor in Digital Humanities courses.</p>
+            <p>Helena is interested in data modeling and the formalization of annotation
+                schemes. She is proficient in XML technologies and linked data. Thanks to
+                her work in this project, she is exploring further programming
+                languages.</p>
+            <p>At WoPoss, she’s contributing to the annotation model. She supervises the
+                technical aspects of the annotation workflow, including the automation of
+                corpus pre-processing and post-processing. She is the developer of this
+                website, so <a href="mailto:wwwwoposs@unil.ch">let her know if you find
+                    anything amiss</a>.</p>
+        </div>
+        <div>
             <figure>
-            <figcaption>Séverine Reymond</figcaption>
+                <img src="images/team/paola.jpg" alt="Paola Marongiu"/>
+                <figcaption>Paola Marongiu (MA 2018)</figcaption>
             </figure>
-            </div>
-            </div>
-        </main>
-        <?php include("ssi/footer.html"); ?>
-    </body>
+            <p>Paola Marongiu is a PhD student in Linguistics at the University of
+                Lausanne, Section des Sciences du Langage et de l’Information, and Assistant
+                in the FNS project WoPoss. Her PhD thesis focuses on the analysis of the
+                co-occurrence of modal markers in Latin, from both a quantitative and a
+                qualitative point of view. Her research interests include Theoretical
+                Linguistics, with a focus on Latin and Modality, Digital Humanities and
+                Computational Linguistics. More specifically, in her PhD project she will
+                employ computational methods and resources in order to investigate a
+                theoretical problem on multiple perspectives.</p>
+            <p>In 2018 she obtained a Master degree in Theoretical and Applied Linguistics
+                from the University of Pavia, with a thesis focused on the conversion of the
+                Index Thomisticus Treebank into the Universal Dependencies annotation style.</p>
+            <p>She has a Bachelor in Modern Studies, obtained from the University of Bologna
+                in 2016.</p>
+
+        </div>
+
+    </div>
+    <h2>Assistants</h2>
+    <div class="members_et">
+        <div>
+            <figure>
+                <img src="images/team/kelvin.jpg" alt="Kelvin Bruggmann"/>
+                <figcaption>Kelvin Bruggmann</figcaption>
+            </figure>
+            <p>Kelvin Bruggmann is a Bachelor (BA) student at the University of Lausanne. His interests are multiple but
+                centered on Latin language and literature and (historical) linguistics.
+                He wants to develop and acquire new abilities and skills in the fields he is studying. His main goal is
+                to better understand ancient cultures, including their languages, in order to better
+                understand our current world. For all those reasons, he is taking part and contributing to this
+                project.</p>
+        </div>
+        <div>
+            <figure>
+                <img src="images/team/mael.jpg" alt="Maël Graa"/>
+                <figcaption>Maël Graa (BA 2018)</figcaption>
+            </figure>
+
+            <p>Maël Graa obtained a Bachelor of Arts in History and Latin and he decided to deepen his knowledge in
+                these two fields during his post-graduate training.
+                He is currently pursuing a Master degree at the Université de Lausanne, with an additional
+                specialization in Discourse and Public Communication Analysis. His Master Thesis concerns the Latin
+                poetry by Théodore de Bèze.</p>
+        </div>
+        <div>
+            <figure>
+                <img src="images/team/severine.jpg" alt="Séverine Reymond">
+                <figcaption>Séverine Reymond (BM 2019)</figcaption>
+            </figure>
+            <p>Séverine Reymond obtained her Bachelor in Music at the Haute Ecole de Musique de
+                Lausanne. Due to her keen interest in interdisciplinarity and having in mind broadening her
+                horizons, she chose to complete her BA in Letters, specializing in Latin and Theology. Her curiosity for
+                linguistics and the opportunity to read complete Latin works have motivated her to be part of this
+                project. She focuses her readings on Christian texts, thereby deepening her knowledge of
+                the history and theology of ancient Christianity.</p>
+        </div>
+    </div>
+</main>
+<?php include("ssi/footer.html"); ?>
+</body>
 </html>
