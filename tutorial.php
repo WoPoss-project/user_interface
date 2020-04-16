5a66f154b0710bf1d88a8c2be491a907514ab2ce
helenasabel <helena.b.sabel@gmail.com>
2020-04-16 11:37:28 +0200

    added video tutorial inception

---
+++ tutorial.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>INCEpTION tutorial</title>
+    <?php include("ssi/head.html"); ?>
+</head>
+<body>
+<?php include("ssi/menu.html"); ?>
+<main>
+    <h1>Annotation tutorial</h1>
+    <div class="video">
+        <video width="1000" height="562.5" controls>
+            <source src="images/woposs-inception.mp4" type="video/mp4"/>
+            Your browser does not support the video tag.
+        </video>
+    </div>
+
+</main>
+
+
+<?php include("ssi/footer.html"); ?>
+
+</body>
+</html>
