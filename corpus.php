632ce71a86e417c3d463e8acbb8f333c4571d4e6
helenasabel <helena.b.sabel@gmail.com>
2020-05-15 15:41:14 +0200

    [data] added de spectaculis annotation

--- corpus.php
+++ corpus.php
@@ -1,18 +1,35 @@
-<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>Corpus - WoPoss</title>
-<?php include("ssi/head.html"); ?>
-        
-        
-    </head>
-    <body>
-        <?php include("ssi/menu.html"); ?>
-        <main>
-            <h1>Corpus description</h1>
-            <div class="warning">This section is under construction</div>
-        </main>
-        <?php include("ssi/footer.html"); ?>
-    </body>
+<head>
+    <title>Corpus - WoPoss</title>
+    <?php include("ssi/head.html"); ?>
+
+
+</head>
+<body>
+<?php include("ssi/menu.html"); ?>
+<main>
+    <h1>Corpus description</h1>
+    <p>The WoPoss corpus spans from the 3rd century BCE to the 7th century CE. Texts are selected in terms of
+        representativeness in order to track the variations in the uses of modal markers: the typological diversity of
+        texts
+        guarantee that any potential source of knowledge about (socio-)linguistic variation is covered. Thus, the corpus
+        includes both literary and documentary texts taking into consideration the diverse sources of transmission of
+        ancient texts: manuscripts, inscriptions and papyri, in the various Latin-speaking regions of the ancient world.
+        Different textual genres are also included: philosophical texts like <em>De Rerum Natura</em> by Lucretius, comedian
+        pieces
+        like <em>Poenulus</em> by Plautus or technical writing like the
+        <em>Res rustica</em> by Columella.</p>
+
+    <p>The setting-up of the corpus builds upon projects that provide open access materials (see the credits page for
+        some
+        of these projects with whom we have either informal and formal agreements). A full description of the annotated
+        corpus will be provided soon.</p>
+
+    <p>A preliminary mock-up of the semantic annotation of <em>De spectaculis</em> by Tertullian is available in <a
+                href="db.php" target="_blank">Query
+            the corpus</a>.</p>
+</main>
+<?php include("ssi/footer.html"); ?>
+</body>
 </html>
--- db.php
+++ db.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
@@ -11,7 +11,27 @@
         <?php include("ssi/menu.html"); ?>
         <main>
             <h1>Query the corpus</h1>
-            <div class="warning">This section is under construction</div>
+            <p>The corpus spans
+                from the 3rd
+                BCE to the 7th
+                century CE, and besides the different textual types and genres, we
+                also took into consideration the diverse sources of transmission of ancient texts.</p>
+            <p>This section will provide a search interface to access our annotated corpus. Meanwhile, you
+            can have a glimpse of our annotation by visualizing Tertullian’s <em>De spectaculis</em> as annotated by Séverine Reymond and
+                reviewed by Francesca Dell’Oro in INCEpTION. Please notice that this is not the final annotated file: it is the result
+                of the an automatic annotation of linguistic features and the manual annotation of modal passages. More metadata and corrections
+                need to be added in following steps.</p>
+            <p>Instructions:
+            </p>
+            <ol>
+                <li>Download the <a href="other/copy_of_WoPoss_project_2020-05-14_0929.zip" target="_blank">file</a>.</li>
+                <li>Go to the <a href="https://morbo.ukp.informatik.tu-darmstadt.de/demo" target="_blank">INCEpTION demo</a> website.</li>
+                <li>After logging in, go to the right upper corner and in “Import project files” select the file you downloaded in step 1 and click on “Upload selected files”.</li>
+                <li>Once the project was correctly imported, pay attention to green pop-up stating the name of the uploaded project (as default the name is “copy_of_WoPoss” but if
+                    other users have previously uploaded and not deleted the file, the name will change to avoid a rewriting). Select it on the list of projects to open it.</li>
+                <li>Go to “Annotation” to overview the semantic annotation of <em>De spectaculis</em>.</li>
+                <li>After you finish, you may go back to the dashboard (upper menu), select “Settings” and click of the red button “Delete” (upper right corner).</li>
+            </ol>
         </main>
         <?php include("ssi/footer.html"); ?>
     </body>
