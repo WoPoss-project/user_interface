30ae58f721b6f77a55db0c43d32a6de988df706a
helenasabel <helena.b.sabel@gmail.com>
2021-02-11 13:23:01 +0100

    reformat json files

--- js/json/indubitatus.js
+++ js/json/indubitatus.js
@@ -1,1 +1,96 @@
-map_data = {"normalForm":true,"headword":"INDUBITATUS","etymology":[{"period":"PIE","form":"PIE *dui-bʰh₂u-o-","def":"'double'","certitude":true},{"period":"PIt","form":"du(i)-fwo-","def":"","certitude":true}],"dataFormat":"cent","meanings":[{"definition":"Undoubted","construct":"_","group":"_","modalities":[{"id":"tcswk","modal":"Not modal","emergence":-1,"disparition":"None","attestation":"SEN. dial. 6, 20, 4 si Cn. Pompeium ... Neapoli valetudo abstulisset, indubitatus populi R. princeps excesserat.","certainty":true,"relationships":{"origins":[],"destinations":[{"rel":"jkiamjo","cert":true},{"rel":"jisgsc","cert":true},{"rel":"yrgvxt","cert":true},{"rel":"vzrqjv","cert":true}],"unspecified":[]}},{"id":"jkiamjo","modal":"Modal: epistemic","emergence":1,"disparition":"None","attestation":"PLIN. nat. 2, 63 omnia autem haec constant ratione circini semper indubitata.","certainty":true,"relationships":{"origins":[{"rel":"tcswk","cert":true}],"destinations":[],"unspecified":[{"rel":"izsycs","cert":true}]}}]},{"definition":"Certain, solid (referred to reasoning or knowledge)","construct":"_","group":"_","modalities":[{"id":"jisgsc","modal":"Not modal","emergence":1,"disparition":"None","attestation":"PLIN. nat. 6, 172 cum indubitata ratione umbrarum Eratosthenes mensuram terrae prodere inde conceperit.","certainty":true,"relationships":{"origins":[{"rel":"tcswk","cert":true}],"destinations":[],"unspecified":[]}}]},{"definition":"Not ambiguous","construct":"_","group":"_","modalities":[{"id":"vzrqjv","modal":"Not modal","emergence":4,"disparition":"None","attestation":"HIL. syn. 39 post ... illam ... indubitatam sacramentorum expositionem.","certainty":true,"relationships":{"origins":[{"rel":"tcswk","cert":true}],"destinations":[],"unspecified":[]}}]},{"definition":"It is undoubted","construct":"INDUBITATUM EST","group":"_","modalities":[{"id":"izsycs","modal":"Modal: epistemic","emergence":1,"disparition":"None","attestation":"PLIN. nat. 7, 156 Massinissam LX annis regnasse indubitatum est.","certainty":true,"relationships":{"origins":[],"destinations":[],"unspecified":[{"rel":"jkiamjo","cert":true}]}}]},{"definition":"Steady","construct":"_","group":"_","modalities":[{"id":"yrgvxt","modal":"Not modal","emergence":1,"disparition":"None","attestation":"PLIN. nat. 22, 103 extera corporum indubitatas confessiones habent (laser).","certainty":true,"relationships":{"origins":[{"rel":"tcswk","cert":true}],"destinations":[],"unspecified":[]}}]}]}
+map_data = {
+    "normalForm": true,
+    "headword": "INDUBITATUS",
+    "etymology": [{
+        "period": "PIE",
+        "form": "PIE *dui-bʰh₂u-o-",
+        "def": "'double'",
+        "certitude": true
+    }, {"period": "PIt", "form": "du(i)-fwo-", "def": "", "certitude": true}],
+    "dataFormat": "cent",
+    "meanings": [{
+        "definition": "Undoubted",
+        "construct": "_",
+        "group": "_",
+        "modalities": [{
+            "id": "tcswk",
+            "modal": "Not modal",
+            "emergence": -1,
+            "disparition": "None",
+            "attestation": "SEN. dial. 6, 20, 4 si Cn. Pompeium ... Neapoli valetudo abstulisset, indubitatus populi R. princeps excesserat.",
+            "certainty": true,
+            "relationships": {
+                "origins": [],
+                "destinations": [{"rel": "jkiamjo", "cert": true}, {"rel": "jisgsc", "cert": true}, {
+                    "rel": "yrgvxt",
+                    "cert": true
+                }, {"rel": "vzrqjv", "cert": true}],
+                "unspecified": []
+            }
+        }, {
+            "id": "jkiamjo",
+            "modal": "Modal: epistemic",
+            "emergence": 1,
+            "disparition": "None",
+            "attestation": "PLIN. nat. 2, 63 omnia autem haec constant ratione circini semper indubitata.",
+            "certainty": true,
+            "relationships": {
+                "origins": [{"rel": "tcswk", "cert": true}],
+                "destinations": [],
+                "unspecified": [{"rel": "izsycs", "cert": true}]
+            }
+        }]
+    }, {
+        "definition": "Certain, solid (referred to reasoning or knowledge)",
+        "construct": "_",
+        "group": "_",
+        "modalities": [{
+            "id": "jisgsc",
+            "modal": "Not modal",
+            "emergence": 1,
+            "disparition": "None",
+            "attestation": "PLIN. nat. 6, 172 cum indubitata ratione umbrarum Eratosthenes mensuram terrae prodere inde conceperit.",
+            "certainty": true,
+            "relationships": {"origins": [{"rel": "tcswk", "cert": true}], "destinations": [], "unspecified": []}
+        }]
+    }, {
+        "definition": "Not ambiguous",
+        "construct": "_",
+        "group": "_",
+        "modalities": [{
+            "id": "vzrqjv",
+            "modal": "Not modal",
+            "emergence": 4,
+            "disparition": "None",
+            "attestation": "HIL. syn. 39 post ... illam ... indubitatam sacramentorum expositionem.",
+            "certainty": true,
+            "relationships": {"origins": [{"rel": "tcswk", "cert": true}], "destinations": [], "unspecified": []}
+        }]
+    }, {
+        "definition": "It is undoubted",
+        "construct": "INDUBITATUM EST",
+        "group": "_",
+        "modalities": [{
+            "id": "izsycs",
+            "modal": "Modal: epistemic",
+            "emergence": 1,
+            "disparition": "None",
+            "attestation": "PLIN. nat. 7, 156 Massinissam LX annis regnasse indubitatum est.",
+            "certainty": true,
+            "relationships": {"origins": [], "destinations": [], "unspecified": [{"rel": "jkiamjo", "cert": true}]}
+        }]
+    }, {
+        "definition": "Steady",
+        "construct": "_",
+        "group": "_",
+        "modalities": [{
+            "id": "yrgvxt",
+            "modal": "Not modal",
+            "emergence": 1,
+            "disparition": "None",
+            "attestation": "PLIN. nat. 22, 103 extera corporum indubitatas confessiones habent (laser).",
+            "certainty": true,
+            "relationships": {"origins": [{"rel": "tcswk", "cert": true}], "destinations": [], "unspecified": []}
+        }]
+    }]
+}
--- js/json/licitus.js
+++ js/json/licitus.js
@@ -1,1 +1,50 @@
-map_data = {"normalForm":true,"headword":"LICITUS, -a, -um","etymology":[{"period":"PIt","form":"lik-ē-","def":"","certitude":true}],"dataFormat":"cent","meanings":[{"definition":"Allowed, lawful","construct":"_","group":"_","modalities":[{"id":"nukbce","modal":"Not modal","emergence":-1,"disparition":"None","attestation":"VERG. Aen. 8, 468 licito tandem sermone fruuntur.","certainty":true,"relationships":{"origins":[],"destinations":[{"rel":"bidsbwr","cert":true}],"unspecified":[{"rel":"bkuiw","cert":true}]}}]},{"definition":"It is allowed","construct":"LICITUM EST","group":"_","modalities":[{"id":"bidsbwr","modal":"Modal: deontic","emergence":5,"disparition":"None","attestation":"COD. IUST. 1, 57, 1 Iubemus apud Alexandrinae dumtaxat clarissimae civitatis iuridicum licitum et concessum esse singulis quibusque volentibus donationis conscriptae sollemniter instrumenta reserare.","certainty":true,"relationships":{"origins":[{"rel":"nukbce","cert":true}],"destinations":[],"unspecified":[]}}]},{"definition":"What is allowed","construct":"_","group":"_","modalities":[{"id":"bkuiw","modal":"Not modal","emergence":-1,"disparition":"None","attestation":"MANIL. 4, 211 licitum sciet et vetitum quae poena sequatur.","certainty":true,"relationships":{"origins":[],"destinations":[],"unspecified":[{"rel":"nukbce","cert":true}]}}]}]}
+map_data = {
+    "normalForm": true,
+    "headword": "LICITUS, -a, -um",
+    "etymology": [{"period": "PIt", "form": "lik-ē-", "def": "", "certitude": true}],
+    "dataFormat": "cent",
+    "meanings": [{
+        "definition": "Allowed, lawful",
+        "construct": "_",
+        "group": "_",
+        "modalities": [{
+            "id": "nukbce",
+            "modal": "Not modal",
+            "emergence": -1,
+            "disparition": "None",
+            "attestation": "VERG. Aen. 8, 468 licito tandem sermone fruuntur.",
+            "certainty": true,
+            "relationships": {
+                "origins": [],
+                "destinations": [{"rel": "bidsbwr", "cert": true}],
+                "unspecified": [{"rel": "bkuiw", "cert": true}]
+            }
+        }]
+    }, {
+        "definition": "It is allowed",
+        "construct": "LICITUM EST",
+        "group": "_",
+        "modalities": [{
+            "id": "bidsbwr",
+            "modal": "Modal: deontic",
+            "emergence": 5,
+            "disparition": "None",
+            "attestation": "COD. IUST. 1, 57, 1 Iubemus apud Alexandrinae dumtaxat clarissimae civitatis iuridicum licitum et concessum esse singulis quibusque volentibus donationis conscriptae sollemniter instrumenta reserare.",
+            "certainty": true,
+            "relationships": {"origins": [{"rel": "nukbce", "cert": true}], "destinations": [], "unspecified": []}
+        }]
+    }, {
+        "definition": "What is allowed",
+        "construct": "_",
+        "group": "_",
+        "modalities": [{
+            "id": "bkuiw",
+            "modal": "Not modal",
+            "emergence": -1,
+            "disparition": "None",
+            "attestation": "MANIL. 4, 211 licitum sciet et vetitum quae poena sequatur.",
+            "certainty": true,
+            "relationships": {"origins": [], "destinations": [], "unspecified": [{"rel": "nukbce", "cert": true}]}
+        }]
+    }]
+}
